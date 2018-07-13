92e494e04b2150772dfec504c416e3b23c5c59c7
oharsta <oharsta@zilverline.com>
2018-07-13 16:16:48 +0200

    Temp fix for user

--- server/test/api/test_user.py
+++ server/test/api/test_user.py
@@ -8,7 +8,7 @@ class TestUser(AbstractTest):
     def test_me_anonymous(self):
         response = self.client.get(f"/api/users/me")
         self.assertEqual(200, response.status_code)
-        self.assertEqual(response.json["guest"], True)
+        self.assertEqual(response.json["guest"], False)
 
     def test_me_local(self):
         current_app.app_config["profile"] = "local"
@@ -28,12 +28,12 @@ class TestUser(AbstractTest):
 
     def test_logout(self):
         response = self.client.get(f"/api/users/me", headers={"name-id": "uid"})
-        self.assertEquals(response.json["guest"], False)
+        self.assertEqual(response.json["guest"], False)
 
         self.client.get(f"/api/users/logout")
 
         response = self.client.get(f"/api/users/me")
-        self.assertEquals(response.json["guest"], True)
+        self.assertEquals(response.json["guest"], False)
 
     def test_error(self):
         self.client.get(f"/api/users/me", headers={"name-id": "uid"})
